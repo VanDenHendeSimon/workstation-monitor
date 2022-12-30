4c20098e4d502fdd1bd69a7d83640cf9b8d73723
Simon Van Den Hende <svandenhende@gantner.com>
2022-12-30 12:00:09 +0100

    [REFACTOR] Move Storage implementation to its own project

--- src/WorkStationMonitor.Console/Program.cs
+++ src/WorkStationMonitor.Console/Program.cs
@@ -1,14 +1,10 @@
-﻿using InfluxDB.Client;
-using InfluxDB.Client.Api.Domain;
-using InfluxDB.Client.Writes;
-using WorkStationMonitor.Core;
+﻿using WorkStationMonitor.Core;
+using WorkStationMonitor.Storage;
 
 var applicationService = new ApplicationService(new ProcessModuleProvider());
+using var storageService = new StorageService();
 
 var cts = new CancellationTokenSource(TimeSpan.FromHours(10));
-using var client = new InfluxDBClient(
-    url: $"http://localhost:{Environment.GetEnvironmentVariable("DOCKER_INFLUXDB_INIT_PORT")}",
-    token: Environment.GetEnvironmentVariable("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN"));
 
 while (!cts.IsCancellationRequested)
 {
@@ -17,16 +13,6 @@ while (!cts.IsCancellationRequested)
     var activeApplication = applicationService.GetActiveApplication();
     if (activeApplication is null) continue;
 
-    var writeApi = client.GetWriteApiAsync();
-    var point = PointData.Measurement("application")
-        .Tag("application_name", activeApplication.Name)
-        .Field("v", activeApplication.Name)
-        .Timestamp(DateTime.UtcNow, WritePrecision.Ms);
-
-    await writeApi.WritePointAsync(
-        point: point,
-        bucket: Environment.GetEnvironmentVariable("DOCKER_INFLUXDB_APPLICATION_BUCKET"),
-        org: Environment.GetEnvironmentVariable("DOCKER_INFLUXDB_INIT_ORG"));
-
+    await storageService.WriteApplicationAsync(activeApplication);
     Console.WriteLine($"{activeApplication.Name}");
 }
---
+++ src/WorkStationMonitor.Storage/StorageService.cs
@@ -0,0 +1,34 @@
+﻿using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Writes;
+using WorkStationMonitor.Core;
+
+namespace WorkStationMonitor.Storage;
+
+public class StorageService : IDisposable
+{
+    private readonly InfluxDBClient _client;
+
+    public StorageService()
+    {
+        _client = new InfluxDBClient(
+            url: $"http://localhost:{Environment.GetEnvironmentVariable("DOCKER_INFLUXDB_INIT_PORT")}",
+            token: Environment.GetEnvironmentVariable("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN"));
+    }
+
+    public Task WriteApplicationAsync(Application application)
+    {
+        var point = PointData.Measurement("application")
+            .Tag("application_name", application.Name)
+            .Field("v", application.Name)
+            .Timestamp(DateTime.UtcNow, WritePrecision.Ms);
+
+        var writeApi = _client.GetWriteApiAsync();
+        return writeApi.WritePointAsync(
+            point: point,
+            bucket: Environment.GetEnvironmentVariable("DOCKER_INFLUXDB_APPLICATION_BUCKET"),
+            org: Environment.GetEnvironmentVariable("DOCKER_INFLUXDB_INIT_ORG"));
+    }
+
+    public void Dispose() => _client.Dispose();
+}
